679d4b128b66bb01d1dc095b3477c53f1ab7988b
Ackuq@ <axel@pettersson.cc>
2020-03-13 21:47:59 +0100

    Add basic auth

--- standpoints/views.py
+++ standpoints/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Standpoint, Party, Subject
 from .serializer import StandpointSerializer, PartySerializer, SubjectSerializer
 from django_filters import rest_framework as filters
@@ -16,13 +16,16 @@ class StandpointView(viewsets.ModelViewSet):
     queryset = Standpoint.objects.all()
     serializer_class = StandpointSerializer
     filterset_class = StandpointFilter
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class PartyView(viewsets.ModelViewSet):
     queryset = Party.objects.all()
     serializer_class = PartySerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class SubjectView(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
