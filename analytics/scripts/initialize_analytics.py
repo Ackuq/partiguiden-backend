67d240a94b3700bea57f319fbf8be810b50b75b3
Ackuq@ <axel@pettersson.cc>
2021-03-08 09:42:01 +0100

    Better typing

--- analytics/scripts/initialize_analytics.py
+++ analytics/scripts/initialize_analytics.py
@@ -6,6 +6,6 @@ from google.analytics.data_v1alpha import AlphaAnalyticsDataClient
 SERVICE_ACCOUNT = json.loads(os.environ.get("ANALYTICS_SERVICE_ACCOUNT", ""))
 
 
-def initialize_analytics():
-    client = AlphaAnalyticsDataClient.from_service_account_info(SERVICE_ACCOUNT)
+def initialize_analytics() -> AlphaAnalyticsDataClient:
+    client: AlphaAnalyticsDataClient = AlphaAnalyticsDataClient.from_service_account_info(SERVICE_ACCOUNT)
     return client
