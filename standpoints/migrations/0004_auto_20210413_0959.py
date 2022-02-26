6409a5829d9bc92805f27c05fcc9956a9442d160
Axel Pettersson <axel@pettersson.cc>
2022-02-26 17:34:46 +0000

    Fix migrations

--- standpoints/migrations/0004_auto_20210413_0959.py
+++ standpoints/migrations/0004_auto_20210413_0959.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('standpoints', '0003_auto_20210307_0932'),
+        ("standpoints", "0003_auto_20210307_0932"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='standpoint',
-            name='link',
-            field=models.CharField(max_length=150, unique=True, verbose_name='Länk'),
+            model_name="standpoint",
+            name="link",
+            field=models.CharField(max_length=150, unique=True, verbose_name="Länk"),
         ),
     ]
