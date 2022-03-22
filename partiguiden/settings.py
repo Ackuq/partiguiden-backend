25408fccbba2790b19fd3fe7722664fc73a74b75
Axel Pettersson <axel@pettersson.cc>
2022-03-22 21:26:05 +0100

    Disable sending transactions

--- partiguiden/settings.py
+++ partiguiden/settings.py
@@ -80,10 +80,11 @@ if "SENTRY_DSN" in os.environ:
     sentry_sdk.init(
         dsn=os.environ["SENTRY_DSN"],
         integrations=[DjangoIntegration()],
-        traces_sample_rate=1.0,
+        # Do not send any transactions
+        traces_sample_rate=0.0,
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True,
+        send_default_pii=False,
         environment=os.environ.get("ENV", "staging"),
     )
 
