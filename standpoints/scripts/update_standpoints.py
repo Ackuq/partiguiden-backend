c66b5084be023fb7d8cb518ffc8afa666c3e3a8e
Axel Pettersson <axel@pettersson.cc>
2022-07-29 12:23:16 +0200

    Improve standpoint update endpoint

--- standpoints/scripts/update_standpoints.py
+++ standpoints/scripts/update_standpoints.py
@@ -10,9 +10,9 @@ from .party_data.get_party_data import get_party_data
 logger = logging.getLogger("standpoints")
 
 
-def _purge_old(party) -> None:
-    logger.info(f"Purging invalid urls for party {party}...")
-    standpoints = list(Standpoint.objects.filter(party=party).values_list("link", flat=True))
+def _purge_old(party: Party) -> None:
+    logger.info(f"Purging invalid urls for party {party.pk}...")
+    standpoints: List[str] = list(Standpoint.objects.filter(party=party).values_list("link", flat=True))
     invalid_urls: List[str] = get_invalid_urls(standpoints)
     if len(invalid_urls) > 0:
         logger.warn("Found invalid ids: {}".format(invalid_urls))
@@ -20,23 +20,22 @@ def _purge_old(party) -> None:
             Standpoint.objects.filter(pk=invalid_url).delete()
 
 
-def _handle_standpoints_update(party_id: str) -> None:
-    party = Party.objects.get(pk=party_id.upper())
-    logger.info(f"Updating standpoints for party {party_id}...")
+def _handle_standpoints_update(party: Party) -> None:
+    logger.info(f"Updating standpoints for party {party.pk}...")
     _purge_old(party)
-    logger.info(f"Fetching new data for party {party_id}...")
-    pages = get_party_data(party_id)
-    logger.info(f"Found {len(pages)} for party {party_id}, writing new entries...")
+    logger.info(f"Fetching new data for party {party.pk}...")
+    pages = get_party_data(party.pk)
+    logger.info(f"Found {len(pages)} for party {party.pk}, writing new entries...")
     for page in pages:
         try:
             existing = Standpoint.objects.get(pk=page.url)
-            logger.info(f"Updating existing entry {page.title} for party {party_id}")
+            logger.info(f"Updating existing entry {page.title} for party {party.pk}")
             existing.title = page.title
             existing.content = page.opinions
             existing.date = date.today()
             existing.save()
         except Standpoint.DoesNotExist:
-            logger.info(f"Creating entry {page.title} for party {party_id}")
+            logger.info(f"Creating entry {page.title} for party {party.pk}")
             Standpoint.objects.create(
                 title=page.title,
                 content=page.opinions,
@@ -45,10 +44,15 @@ def _handle_standpoints_update(party_id: str) -> None:
             )
 
 
-def update_standpoints(party_id: str) -> None:
+def update_standpoints(party: Party) -> None:
+    _handle_standpoints_update(party)
+
+
+def update_standpoints_str(party_id: str) -> None:
     if party_id.lower() == "all":
-        all_ids = list(Party.objects.values_list("id", flat=True))
-        for id in all_ids:
-            _handle_standpoints_update(id)
+        parties = Party.objects.all()
+        for party in parties:
+            _handle_standpoints_update(party)
     else:
-        _handle_standpoints_update(party_id)
+        party = Party.objects.get(pk=party_id.upper())
+        _handle_standpoints_update(party)
